3735fd1ebb1cf9d0d0209891038ec58f309375be
Joseph Humfrey <joseph.humfrey@gmail.com>
2014-08-19 15:30:26 +0100

    Fixed warnings due to 64 bit ints, etc

--- Sources/OAuth2Client/NXOAuth2AccessToken.m
+++ Sources/OAuth2Client/NXOAuth2AccessToken.m
@@ -192,7 +192,7 @@
     result = (__bridge_transfer NSDictionary *)cfResult;
     
     if (status != noErr) {
-        NSAssert1(status == errSecItemNotFound, @"unexpected error while fetching token from keychain: %ld", status);
+        NSAssert1(status == errSecItemNotFound, @"unexpected error while fetching token from keychain: %d", (int)status);
         return nil;
     }
     
@@ -211,7 +211,7 @@
                            nil];
     [self removeFromDefaultKeychainWithServiceProviderName:provider];
     OSStatus __attribute__((unused)) err = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
-    NSAssert1(err == noErr, @"error while adding token to keychain: %ld", err);
+    NSAssert1(err == noErr, @"error while adding token to keychain: %d", (int)err);
 }
 
 - (void)removeFromDefaultKeychainWithServiceProviderName:(NSString *)provider;
@@ -222,7 +222,7 @@
                            serviceName, kSecAttrService,
                            nil];
     OSStatus __attribute__((unused)) err = SecItemDelete((__bridge CFDictionaryRef)query);
-    NSAssert1((err == noErr || err == errSecItemNotFound), @"error while deleting token from keychain: %ld", err);
+    NSAssert1((err == noErr || err == errSecItemNotFound), @"error while deleting token from keychain: %d", (int)err);
 }
 
 #else
